ae26a666bf18c2ec23fa49efdde04783fa696c30
Robert Carlsen <rwcarlsen@gmail.com>
2015-10-04 22:12:15 -0500

    adding html template for a game page

---
+++ game_page.go
@@ -0,0 +1,20 @@
+package main
+
+const gamePage = `
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+<head>
+	<title>pBGG</title>
+    <link href="/style.css" rel="stylesheet" type="text/css">
+</head>
+<body lang="en">
+
+<h2>{{.Name}}</h2>
+
+<p>{{.Description}}</p>
+
+<image>
+    
+</body>
+</html>
+`
